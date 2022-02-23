14f273f1813d3435652b4c6c636233d6b90f4e12
Piyush <53549454+P1YU5H-50N1@users.noreply.github.com>
2022-02-23 11:01:00 +0530

    Update get_data.sh

--- data/get_data.sh
+++ data/get_data.sh
@@ -35,79 +35,79 @@ fi
 ###################################
 ## Datasets
 ###################################
-mkdir -p $DATADIR/datasets/DIV2K
+# mkdir -p $DATADIR/datasets/DIV2K
 
-## DIV2K train HR
-DIV2K_train_HR=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_HR)
-if [ $DIV2K_train_HR=='' ]; then
-    wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip -P /tmp
-    unzip /tmp/DIV2K_train_HR.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2K_train_HR.zip
-fi
+# ## DIV2K train HR
+# DIV2K_train_HR=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_HR)
+# if [ $DIV2K_train_HR=='' ]; then
+#     wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip -P /tmp
+#     unzip /tmp/DIV2K_train_HR.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2K_train_HR.zip
+# fi
 
-## DIV2K valid HR
-DIV2K_valid_HR=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_valid_HR)
-if [ $DIV2K_valid_HR=='' ]; then
-    wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip -P /tmp
-    unzip /tmp/DIV2K_valid_HR.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2K_valid_HR.zip
-fi
+# ## DIV2K valid HR
+# DIV2K_valid_HR=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_valid_HR)
+# if [ $DIV2K_valid_HR=='' ]; then
+#     wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip -P /tmp
+#     unzip /tmp/DIV2K_valid_HR.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2K_valid_HR.zip
+# fi
 
-## DIV2K train 8x
-DIV2K_train_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_LR_bicubic)
-if [ $DIV2K_train_LR_bicubic=='' ]; then
-    wget -nc https://www.dropbox.com/s/uvwtxy5hul90hyl/DIV2KX8.zip -O /tmp/DIV2KX8.zip
-    unzip /tmp/DIV2KX8.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2KX8.zip
-fi
+# ## DIV2K train 8x
+# DIV2K_train_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_LR_bicubic)
+# if [ $DIV2K_train_LR_bicubic=='' ]; then
+#     wget -nc https://www.dropbox.com/s/uvwtxy5hul90hyl/DIV2KX8.zip -O /tmp/DIV2KX8.zip
+#     unzip /tmp/DIV2KX8.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2KX8.zip
+# fi
 
-## DIV2K train LR bicubic x4
-DIV2K_train_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_LR_bicubic)
-if [ $DIV2K_train_LR_bicubic=='' ]; then
-    wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip -P /tmp
-    unzip /tmp/DIV2K_train_LR_bicubic_X2.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2K_train_LR_bicubic_X4.zip
-fi
+# ## DIV2K train LR bicubic x4
+# DIV2K_train_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_LR_bicubic)
+# if [ $DIV2K_train_LR_bicubic=='' ]; then
+#     wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip -P /tmp
+#     unzip /tmp/DIV2K_train_LR_bicubic_X2.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2K_train_LR_bicubic_X4.zip
+# fi
 
-## DIV2K valid LR bicubic x4
-DIV2K_valid_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_valid_LR_bicubic)
-if [ $DIV2K_valid_LR_bicubic=='' ]; then
-    wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip -P /tmp
-    unzip /tmp/DIV2K_valid_LR_bicubic_X2.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2K_valid_LR_bicubic_X4.zip
-fi
+# ## DIV2K valid LR bicubic x4
+# DIV2K_valid_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_valid_LR_bicubic)
+# if [ $DIV2K_valid_LR_bicubic=='' ]; then
+#     wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip -P /tmp
+#     unzip /tmp/DIV2K_valid_LR_bicubic_X2.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2K_valid_LR_bicubic_X4.zip
+# fi
 
-## DIV2K train LR bicubic x2
-DIV2K_train_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_LR_bicubic)
-if [ $DIV2K_train_LR_bicubic=='' ]; then
-    wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X2.zip -P /tmp
-    unzip /tmp/DIV2K_train_LR_bicubic_X4.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2K_train_LR_bicubic_X2.zip
-fi
+# ## DIV2K train LR bicubic x2
+# DIV2K_train_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_train_LR_bicubic)
+# if [ $DIV2K_train_LR_bicubic=='' ]; then
+#     wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X2.zip -P /tmp
+#     unzip /tmp/DIV2K_train_LR_bicubic_X4.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2K_train_LR_bicubic_X2.zip
+# fi
 
-## DIV2K train LR bicubic x2
-DIV2K_valid_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_valid_LR_bicubic)
-if [ $DIV2K_valid_LR_bicubic=='' ]; then
-    wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X2.zip -P /tmp
-    unzip /tmp/DIV2K_valid_LR_bicubic_X4.zip -d $DATADIR/datasets/DIV2K
-    rm /tmp/DIV2K_valid_LR_bicubic_X2.zip
-fi
+# ## DIV2K train LR bicubic x2
+# DIV2K_valid_LR_bicubic=$(ls $DATADIR'/datasets/DIV2k' | grep DIV2K_valid_LR_bicubic)
+# if [ $DIV2K_valid_LR_bicubic=='' ]; then
+#     wget -nc http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X2.zip -P /tmp
+#     unzip /tmp/DIV2K_valid_LR_bicubic_X4.zip -d $DATADIR/datasets/DIV2K
+#     rm /tmp/DIV2K_valid_LR_bicubic_X2.zip
+# fi
 
-# Flickr2K
-Flickr2K_LR_bicubic=$(ls $DATADIR'/datasets/Flickr2K' | grep Flickr2K_LR_bicubic)
-if [ $Flickr2K_LR_bicubic=='' ]; then
-    wget -nc http://cv.snu.ac.kr/research/EDSR/Flickr2K.tar -P /tmp
-    tar -xvf /tmp/Flickr2K.tar -C $DATADIR/datasets/ --keep-newer-files
-    rm -rf /tmp/Flickr2K.tar
-fi
+# # Flickr2K
+# Flickr2K_LR_bicubic=$(ls $DATADIR'/datasets/Flickr2K' | grep Flickr2K_LR_bicubic)
+# if [ $Flickr2K_LR_bicubic=='' ]; then
+#     wget -nc http://cv.snu.ac.kr/research/EDSR/Flickr2K.tar -P /tmp
+#     tar -xvf /tmp/Flickr2K.tar -C $DATADIR/datasets/ --keep-newer-files
+#     rm -rf /tmp/Flickr2K.tar
+# fi
 
-# Set14, Urban100, BSD100
-Urban100=$(ls $DATADIR'/datasets' | grep Urban100)
-Set14=$(ls $DATADIR'/datasets' | grep Set14)
-BSD100=$(ls $DATADIR'/datasets' | grep BSD100)
-if [ $Urban100=='' ] && [ $Set14=='' ] && [ $BSD100=='' ]; then
-    wget -nc https://cv.snu.ac.kr/research/EDSR/benchmark.tar -P /tmp
-    tar -xvf /tmp/benchmark.tar -C /tmp --keep-newer-files
-    mv /tmp/benchmark/* $DATADIR/datasets
-    rm -rf /tmp/benchmark
-fi
+# # Set14, Urban100, BSD100
+# Urban100=$(ls $DATADIR'/datasets' | grep Urban100)
+# Set14=$(ls $DATADIR'/datasets' | grep Set14)
+# BSD100=$(ls $DATADIR'/datasets' | grep BSD100)
+# if [ $Urban100=='' ] && [ $Set14=='' ] && [ $BSD100=='' ]; then
+#     wget -nc https://cv.snu.ac.kr/research/EDSR/benchmark.tar -P /tmp
+#     tar -xvf /tmp/benchmark.tar -C /tmp --keep-newer-files
+#     mv /tmp/benchmark/* $DATADIR/datasets
+#     rm -rf /tmp/benchmark
+# fi
